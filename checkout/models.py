adf8db42fd8ec0c4230f35b850c57b59d5593513
Martina Martin <marti.law@gmail.com>
2022-10-20 09:49:50 +0000

    Create initial checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,81 @@
+import uuid
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+from products.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    """
+    This model will do used to generate
+    the order form on the checkout page
+    """
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=120, null=False, blank=False)
+    email_adress = models.EmailField(max_length=350, null=False, blank=False)
+    phone = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=80, null=True, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    shipping_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+
+    def _generate_order_number(self):
+        """
+        Will create a unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Updates the grand total each time a new item
+        is added and calculates shipping cost
+        """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        if self.order_total < settings.FREE_SHIPPING_THRESHOLD:
+            self.shipping_cost = self.order_total * settings.STANDARD_SHIPPING_PERCENTAGE / 100
+        else:
+            self.shipping_cost = 0
+        self.grand_total = self.order_total + self.shipping_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set
+        the order number if it hasn't been set
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderLineItems(models.Model):
+    """
+    creates a lineitem for each product in the 
+    order
+    """
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set
+        the lineitem total and update order total
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order number {self.order.order_number}'
