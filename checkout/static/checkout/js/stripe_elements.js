c622519e7c369b3fe6d330659b641033cc3ca635
Martina Martin <marti.law@gmail.com>
2022-10-26 10:07:09 +0000

    Add initial functionality to checkout

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,7 +1,7 @@
 /* Logic for Stripe payments */
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe('pk_test_51LdYGOGScmbyNe5yNX8o0lHL9e3h9ijtPvKgMMZkoamDFGadEWOL6E03UFQewAsnEEO8VBdWRuhmPPuHCaaw0kdQ00LxW4XcTV');
 var elements = stripe.elements();
 var style = {
@@ -21,3 +21,46 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+/* Handles realtime validation on card elements */
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+        <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = ''
+    }
+});
+
+/* Handle form submission */
+
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({'disable': true});
+    $('#submit-button').attr('disable', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+        var html = `
+        <span>${result.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+        card.update({'disable': false});
+        $('#submit-button').attr('disable', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.onsubmit();
+            }
+        }
+        
+    })
+})
--- checkout/views.py
+++ checkout/views.py
@@ -1,18 +1,43 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
+import stripe
+from basket.contexts import basket_contents
 from .forms import OrderForm
 
 
 def checkout(request):
+    """
+    Checkout View
+    """
+    stripe_public_key = settings.SRTIPE_PUBLIC_KEY
+    stripe_secret_key = settings.SRTIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, 'There is nothing in your basket!')
         return redirect(reverse('index'))
 
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'You are missing your public key. \
+            Please make sure it is set!')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
