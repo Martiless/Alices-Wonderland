c166f5fd768a2c224d7cc36ef1dacf917329f043
Martina Martin <marti.law@gmail.com>
2022-10-31 19:25:00 +0000

    Initial changes to profile app

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 
 def user_profile(request):
@@ -6,8 +7,12 @@ def user_profile(request):
     The view to display
     the user's profile
     """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile
+    }
 
     return render(request, template, context)
 
