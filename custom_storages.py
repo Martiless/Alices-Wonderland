96cb44a6f722fe37aff573c0463395af265c099f
Martina Martin <marti.law@gmail.com>
2022-11-03 18:14:24 +0000

    Set up AWS storage in settings.py

---
+++ custom_storages.py
@@ -0,0 +1,11 @@
+#Code taken from CI Walkthrough project 
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticStorage(S3Boto3Storage):
+    location = settings.STATICFILES_LOCATION
+
+
+class MediaStorage(S3Boto3Storage):
+    location = settings.MEDIAFILES_LOCATION
