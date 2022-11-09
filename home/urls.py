06889fe08c92f3470df3e420e80bd2384ae3bb7f
Martina Martin <marti.law@gmail.com>
2022-11-09 11:46:33 +0000

    Add privacy_policy.html file

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about_me/', views.about_me, name='about_me'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,9 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    """
+    Renders the home page
+    """
 
     return render(request, 'home/index.html')
 
 def about_me(request):
+    """
+    Renders the about me page
+    """
 
     return render(request, 'home/about_me.html')
+
+def privacy_policy(request):
+    """
+    Renders the privacy policy page
+    """
+
+    return render(request, 'home/privacy_policy.html')
